b9dfb328080fb91fdf16f2842bea68168eeda5e6
romashka <roma.l@mail.ru>
2022-09-29 17:38:47 +0400

    with ean

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import qrcode, PIL
+import qrcode, PIL, barcode
 
 img = qrcode.make('some')
 img.save('save.png')
+
+ean = barcode.get_barcode_class('ean13')
+ean2 = ean('123456789123')
+ean2.save('ean_test')
 
