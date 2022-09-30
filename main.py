aace6e6262a327142cfd56df38a8d21699dd6a9e
romashka <roma.l@mail.ru>
2022-09-30 10:08:39 +0400

    reminder

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import qrcode, PIL, barcode
 
-img = qrcode.make('some')
-img.save('save.png')
+img = qrcode.make('https://gelta.ru/')
+img.save('gelta.png')
 
 ean = barcode.get_barcode_class('ean13')
 ean2 = ean('123456789123')
-ean2.save('ean_test')
+ean2.save('ean_test2')
 
