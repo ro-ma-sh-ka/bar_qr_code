334f19220c8d2f9b6e7bbf49c4ba56592fa6e096
romashka <roma.l@mail.ru>
2022-10-09 21:19:18 +0400

    2pys deleted

--- bar_code.py
+++
@@ -1,6 +0,0 @@
-def print_barcode(items, barcode_type='ean13', folder_to_save=None):
-    import barcode
-    for item in items:
-        file_to_save = str(folder_to_save) + '/' + item['article']
-        text = f"Art: {item['article']}\\n adress: {item['country']}, {item['adress']}, warranty: {item['warranty']}"
-        barcode.generate(barcode_type, item['barcode'], None, file_to_save, None, text)
